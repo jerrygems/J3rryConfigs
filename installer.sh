ce69a9e0955b83233ebb854a936a03731d263a9e
Shubham Jangid <100356532+jerrygems@users.noreply.github.com>
2023-08-17 06:19:38 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install i3 cava playerctl neofetch ranger polybar picom alacritty btop pulsemixer
+sudo apt install i3-wm cava playerctl neofetch ranger polybar picom alacritty btop pulsemixer
 pip install lyrics-in-terminal
