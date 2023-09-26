cdb4531099d85966e4b32b96b17c5cef66699a3c
jerrygems <shubham.debian@gmail.com>
2023-09-26 20:58:09 +0530

    added few things not so much

--- installer.sh
+++ installer.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install i3-wm cava blueman playerctl neofetch ranger polybar picom alacritty btop pulsemixer acpi bpython feh
+sudo apt install i3-wm cava blueman playerctl neofetch ranger polybar picom alacritty btop pulsemixer acpi bpython feh rofi brightnessctl
 pip install lyrics-in-terminal
